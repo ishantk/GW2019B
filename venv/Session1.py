aabffe18a4f728dae8a8f9ad6dc07ba0f4368f0a
Ishant Kumar <er.ishant@gmail.com>
2019-03-11 18:28:49 +0530

    Image and Notes

--- venv/Session1.py
+++ venv/Session1.py
@@ -2,3 +2,88 @@ a = 10
 b = 20
 c = a+b
 print("c is:",c)
+
+
+"""
+
+Python 			| PL 
+Data Science / Analysis on Data
+Machine Learning
+AI Intro
+
+
+Python as PL !!
+1. What is a PL ? Why we need to use Python if world has so many other Programming Languages ?
+
+PL is used to create a software !!
+software solves a problem !!
+	application software
+		eg: MS Word
+	system software
+		eg: Drivers
+
+
+Computer
+	Compute -> Maths
+
+	110 + 220 = 330
+	110 + 220 + 330 + 440 + 550 = ?
+	17236 + 554561+ 776512 + 890112 + 97679 = ?
+
+	When we wish to save time !!
+
+	Software
+		When we wish to save time ands solve complex problems !!
+		A - 65 | ASCII CODES / UNICODES
+
+
+		PL
+
+
+	To Create SW we must write some commands in a file
+	And save the same !!
+
+	Oranges
+
+	Juicer
+
+	Orange Juice
+
+	Packing
+
+	Glass \_/	
+
+	=========
+
+	MyProg.cpp
+
+	Compiler
+
+	MyProg.obj
+
+	Pack It
+
+W	MyProg.exe
+L   MyProg.sh
+M   MyProg.dmg
+
+	==========
+
+	MyProg.java
+
+	Compiler
+
+	MyProg.class
+
+	==========
+
+	MyProg.py
+
+	Thats It !!
+
+
+
+	PyCharm is IDE
+	Software which is used to create Softwares
+
+"""
