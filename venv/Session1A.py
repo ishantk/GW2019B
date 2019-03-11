06d97059ba068c2acad4f833093dbe9911c96d96
Ishant Kumar <er.ishant@gmail.com>
2019-03-11 18:25:47 +0530

    Initial commit

---
+++ venv/Session1A.py
@@ -0,0 +1,16 @@
+
+# Single Value Container
+a = 10
+print(a)
+print(type(a))
+
+# Multi Value Container
+b = 10, 20, 30, 40, 50
+print(b)
+print(type(b))
+
+age = 10
+ages = 10, 13, 45, 21, 67
+
+ldhPop = 1311341
+punjabPop = 1334121, 2343431, 434123, 2435321, 3411234
